938e4460824c444aa6c8cc6d01173fdcb5e174db
cjc <carlclegg@gmail.com>
2013-04-25 17:03:08 +0100

    Settings for live....again

--- Casino/src/org/smokinmils/casino/Settings.java
+++ Casino/src/org/smokinmils/casino/Settings.java
@@ -18,7 +18,7 @@ public class Settings {
 	
 	
 	// LIVE DATA
-	public static String DBHOST = "66.147.232.16";//"localhost";
+	public static String DBHOST = "localhost";
 	public static String DBNAME = "live";
 	public static String BOTNAME = "SM_BOT";
 	
