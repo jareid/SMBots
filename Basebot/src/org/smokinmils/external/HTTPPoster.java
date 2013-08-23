f5b4f1df7da9f0e354e08a5096c6c9587e31e728
cjc <carlclegg@gmail.com>
2013-08-23 10:47:00 +0100

    [FIX] pom.xml for HTTPPost libraries. Checkstyle problems solved.

--- Basebot/src/org/smokinmils/external/HTTPPoster.java
+++ Basebot/src/org/smokinmils/external/HTTPPoster.java
@@ -1,16 +1,12 @@
 package org.smokinmils.external;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.entity.ContentType;
-import org.apache.http.HttpResponse;
 import org.smokinmils.bot.XMLLoader;
 import org.smokinmils.database.types.ProfileType;
 import org.smokinmils.logging.EventLog;
@@ -26,22 +22,22 @@ public class HTTPPoster {
     private static final String JSONMIME = "application/json";
     
     /** Conversion between coins and EOC. */
-    private final int COIN_2_EOC = 100000;
+    private static final int COIN2_EOC = 100000;
     
     /** Conversion between coins and 07. */
-    private final int COIN_2_07 = 10000;
+    private static final int COIN_2_07 = 10000;
     
     /** The TOKEN for submitting data. */
-    private final String TOKEN = XMLLoader.getInstance().getHTTPSetting("token");
+    private static final String TOKEN = XMLLoader.getInstance().getHTTPSetting("token");
     //"9c8fcb2a-0bc2-4288-8880-0e2f3f42598d";
     
     /** URL To post to. */
-    private final String URL = XMLLoader.getInstance().getHTTPSetting("url");
+    private static final String URL = XMLLoader.getInstance().getHTTPSetting("url");
     //"http://22.smokin-goldshop.appspot.com/stock"; 
     //"http://smokin-goldshop.appspot.com/stock";
     
     /** JSON to send a gold transaction. */
-    private final String SUBMIT_GOLD = "{\"token\": \"%token\", \"agent\": \"%agent\", "
+    private static final String SUBMIT_GOLD = "{\"token\": \"%token\", \"agent\": \"%agent\", "
                                     + "\"amount\": %amount, \"comment\": \"\", "
                                     + "\"goldtype\": \"%goldtype\"}";
     
@@ -64,7 +60,7 @@ public class HTTPPoster {
         out = out.replaceAll("%goldtype", prof.getText());
         int sendamount = 0;
         if (prof.equals(ProfileType.EOC)) {
-            sendamount = (int) (amount * COIN_2_EOC);
+            sendamount = (int) (amount * COIN2_EOC);
         } else {
             sendamount = (int) (amount * COIN_2_07);
         }
